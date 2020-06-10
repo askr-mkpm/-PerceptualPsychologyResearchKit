a47121eca071814ce8fe1a596f580b4d96904364
Mikipomaid <cakemas0227@icloud.com>
2020-06-10 15:06:32 +0900

    pause, play

--- pprk/src/component/VideoRenderer.tsx
+++ pprk/src/component/VideoRenderer.tsx
@@ -4,9 +4,24 @@ import ReactPlayer from 'react-player'
 const VideoRenderer: React.FC = () =>
 {
     const [repeatNum, setRepeatNum] = React.useState<string>("https://youtu.be/Kpm1l0HfkV0");
+    const [playBool, setPlayBool] = React.useState<boolean>(true);
+
+    const handlePauseBool = (e: React.FormEvent<HTMLButtonElement>) => 
+    {
+        setPlayBool(false);
+    }
+
+    const handlePlayBool = (e: React.FormEvent<HTMLButtonElement>) => 
+    {
+        setPlayBool(true);
+    }
 
     return(
-        <ReactPlayer url={repeatNum} playing />
+        <div>
+            <ReactPlayer url={repeatNum} playing={playBool} />
+            <button onClick={handlePauseBool}>pause</button>
+            <button onClick={handlePlayBool}>play</button>
+        </div>
     )
 }
 
